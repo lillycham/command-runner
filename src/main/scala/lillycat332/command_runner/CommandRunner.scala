6e56487f3c4bf2212c693f4303be8ddb554ea9db
Lilly <lillycat332@gmail.com>
2023-06-28 21:04:20 +0100

    chore: Update: Use Scala 3 stx

--- src/main/scala/lillycat332/command_runner/CommandRunner.scala
+++ src/main/scala/lillycat332/command_runner/CommandRunner.scala
@@ -1,11 +1,14 @@
 package lillycat332.command_runner
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
+import com.mojang.brigadier
 import com.mojang.brigadier.builder.ArgumentBuilder
-import com.mojang.brigadier.arguments.StringArgumentType
+import com.mojang.brigadier.arguments.StringArgumentType.{greedyString, getString}
 import com.mojang.brigadier.LiteralMessage
 import org.quiltmc.loader.api.ModContainer
 import net.minecraft.server.command.CommandManager.*
+import net.minecraft.command.CommandBuildContext
+import net.minecraft.text.Text
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer
 import org.quiltmc.qsl.base.api.event.Event
 import org.quiltmc.qsl.command.api
@@ -16,24 +19,30 @@ import org.slf4j.LoggerFactory
 import scala.language.postfixOps
 import sys.process.*
 
-object CommandRunner {
+object CommandRunner:
   val LOGGER: Logger = LoggerFactory.getLogger("Command Runner")
-}
 
-class CommandRunner extends ModInitializer {
-  override def onInitialize(mod: ModContainer): Unit = {
+
+class CommandRunner extends ModInitializer:
+  override def onInitialize(mod: ModContainer): Unit =
     CommandRunner.LOGGER.info("Hello Quilt world from {}!", mod.metadata.name)
-    CommandRegistrationCallback.EVENT.register((dispatcher, ctx, env) => {
-      dispatcher.register(literal("runcmd")
-        .requires(source => source.hasPermissionLevel(4))
-        .`then`(argument("command", StringArgumentType.greedyString())
-          .executes(ctx => runServerCommand(StringArgumentType.getString(ctx, "command"))))
-      )
-    })
-  }
-}
+    CommandRegistrationCallback.EVENT.register((dispatcher, ctx, env) =>
 
-val runServerCommand = (command: String) => {
+      val serverCommandExecutor = ctx =>
+        val result = runServerCommand(getString(ctx, "command"))
+        ctx.getSource.sendFeedback(() => Text.literal("Command ran with code: $result"), false)
+        result
+
+      val command = literal("runcmd")
+        .requires(source => source.hasPermissionLevel(4))
+        .`then`(argument("command", greedyString())
+          .executes(serverCommandExecutor)
+        )
+
+      dispatcher.register(command)
+    )
+
+val runServerCommand = (command: String) =>
   CommandRunner.LOGGER.info("Executing {}", command)
   val result = command !
 
@@ -41,7 +50,7 @@ val runServerCommand = (command: String) => {
     CommandRunner.LOGGER.warn("Command {} failed to run! Code: {}", command, result)
   else
     CommandRunner.LOGGER.info("Ran command {}", result)
-  end if
 
   result
-}
+
+
